193c75ddf58c18a8d51da117b0e5bc2bbe3ac771
_vanchieeu <chieuvanbn@gmail.com>
2022-11-18 23:26:10 +0700

    susscess 2/10

--- source.c
+++ source.c
@@ -2,13 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LENGTH 31
+#define HASH_SIZE 31
+
+typedef struct NEWYEAR {
+    char a[MAX_LENGTH];
+
+    struct NEWYEAR *next;
+} NEWYEAR;
+
+NEWYEAR *head[HASH_SIZE];
+
 int hash_func(char *a) {
-    int x = 0;
+    int index = 0;
 
-    for (int i = 0; a[i] != '\0'; i++)
-        x += (int)a[i];
+    for (int i = 0; *(a+i) != '\0'; i++)
+        index += (int)*(a+i);
 
-    return x;
+    return index % HASH_SIZE;
+}
+
+void InsertHash(char *a) {
+    int index = hash_func(a);
+    if (head[index] != NULL)
+        return;
+
+    NEWYEAR *ptr = (NEWYEAR *)malloc(sizeof(NEWYEAR));
+    strcpy(ptr->a, a);
+
+    head[index] = ptr;
 }
 
 int main() {
@@ -16,22 +38,20 @@ int main() {
     scanf("%d", &n);
     getchar();
 
-    int arr[2701];
-    for (int i = 0; i < 2701; i++)
-        arr[i] = 0;
-    for (int i = 1; i <= n; i++) {
-        char a[31];
-        fgets(a, sizeof(a), stdin);
-        a[strlen(a)] = '\0';
+    for (int i = 0; i < n; i++) {
+        char string[MAX_LENGTH];
+        fgets(string, sizeof(string), stdin);
 
-        arr[hash_func(a)%900]++;
+        string[strlen(string)-1] = '\0';
+
+        InsertHash(string);
     }
 
     int d = 0;
-    for (int i = 0; i < 2701; i++)
-        if (arr[i] == 1)
+    for (int i = 0; i < HASH_SIZE; i++)
+        if (head[i] != NULL)
             d++;
-
+    
     printf("%d", d);
     return 0;
 }
